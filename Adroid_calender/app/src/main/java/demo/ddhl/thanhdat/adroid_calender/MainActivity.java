8f5f87d086d9ad176c9d2792c8011108ee4e26a8
MyPC\PC <thanhdatc1xps12@gmail.com>
2017-07-23 09:29:38 +0700

    123

---
+++ Adroid_calender/app/src/main/java/demo/ddhl/thanhdat/adroid_calender/MainActivity.java
@@ -0,0 +1,13 @@
+package demo.ddhl.thanhdat.adroid_calender;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
